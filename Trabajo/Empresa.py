b326857e7aac5d24856a08410041bc215d34fb99
Maria <you@example.com>
2026-02-12 16:00:58 +0100

    Primeras clases y pruebas

--- Trabajo/Empresa.py
+++ Trabajo/Empresa.py
@@ -1,10 +1,18 @@
 class Empresa: 
+    registro = []
+    codigos = set() #para que no haya repetidos 
     def __init__(self, nombre, codigo):
-        self.nombre = nombre
-        self.codigo = codigo
-        self.trabajadores = []
-        Empresa.registro.append(self)
+        
+        if codigo in Empresa.codigos:
+            raise ValueError(f"El código {codigo} ya está registrado")
+        else: 
+            self.nombre = nombre
+            self.codigo = codigo
+            self.trabajadores = []
+            Empresa.registro.append((self.nombre, self.codigo))
+            Empresa.codigos.add(self.codigo)
+            print("Empresa creada")
 
     def contrata(self, empleado):
-        self.trabajadores.append(empleado)
-        print("Ha contratado a", empleado.nombre)
+        self.trabajadores.append((empleado.nombre, empleado.DNI))
+        print(self.nombre, "ha contratado a", empleado.nombre)
--- Trabajo/Persona.py
+++ Trabajo/Persona.py
@@ -1,10 +1,21 @@
 class Persona: 
-    def __init__(self, nombre, DNI, correo): 
-        self.nombre = nombre
-        self.DNI = DNI
-        self.correo = correo
+    registro = []
+    documentos = set()
+    def __init__(self, nombre, DNI): 
+        
+        if DNI in Persona.documentos: 
+            raise ValueError(f"El DNI {DNI} ya está registrado")
+        else: 
+            self.nombre = nombre
+            self.DNI = DNI
+            Persona.documentos.add(DNI)
+            print("Persona creada")
 
 class Cliente(Persona): 
+    def __init__(self, nombre, DNI):
+        super().__init__(nombre,DNI)
+        Persona.registro.append((self.nombre, "Cliente"))
+
     def compra(self, objeto, empresa, empleado):
         if not insinstance(empresa, Empresa):
             print("No es una empresa")
@@ -15,8 +26,17 @@ class Cliente(Persona):
                 print("Ha comprado", objeto, "de", empresa.nombre, "y se lo vendió", empleado.nombre)
 
 class Empleado(Persona):
+    def __init__(self, nombre, DNI): 
+        super().__init__(nombre, DNI)
+        Persona.registro.append((self.nombre, "Empleado"))
     def vende(self, objeto, cliente): 
         print("Ha vendido", objeto, "a", cliente.nombre)
 
     def trabaja(self, empresa): 
-        print("Trabaja para", empresa.nombre)
+        if not insinstance(empresa, Empresa):
+            print("No es una empresa")
+        else:
+            if empresa not in Empresa.registro: 
+                print("Empresa no encontrada")
+            else: 
+                print("Trabaja para", empresa.nombre)
